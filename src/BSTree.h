616a695ea1454a30749617e8a4dbb172d0d275b4
Lucca Carvalho Magalhaes <lcarvalhomagalha@ufl.edu>
2026-03-27 11:20:16 -0400

    Added BSTMovie Interface

---
+++ src/BSTree.h
@@ -0,0 +1,17 @@
+#ifndef BST_H
+#define BST_H
+
+#include "Movie.h"
+#include <vector>
+
+// BST interface
+class BSTMovie {
+    public:
+        virtual bool insert(const Movie& movie) = 0;
+        virtual Movie* searchByMovieID(int64_t movieID) = 0;
+        virtual std::vector<Movie> levelOrderTraversal() = 0;
+        virtual bool isEmpty() = 0;
+
+};
+
+#endif // BST_H
